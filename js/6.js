f0435559fea9b130a68c3faf895c0b25c912b272
rednaxela700 <ohleg@windowslive.com>
2019-08-12 09:50:50 +0200

    6 complete

--- js/6.js
+++ js/6.js
@@ -1,7 +1,9 @@
-// this is an inline comment
-/*
-this is
-a
-multiline
-comment.
- */
+// Declarations
+var studlyCapVar = 10;
+var properCamelCase = "A String";
+var titleCaseOver = 9000;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase= "A String";
+titleCaseOver= 9000;
