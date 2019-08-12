49cc929cd020608725dfb6de36ce077f961f297a
rednaxela700 <ohleg@windowslive.com>
2019-08-12 11:19:05 +0200

    array pop()

--- js/41.js
+++ js/41.js
@@ -1,12 +1,10 @@
 // Example
-var firstLetterOfFirstName = "";
-var firstName = "Ada";
-
-firstLetterOfFirstName = firstName[0];
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
 
 // Setup
-var firstLetterOfLastName = "";
-var lastName = "Lovelace";
+var myArray = [["John", 23], ["cat", 2]];
 
-// Only change code below this line
-firstLetterOfLastName = lastName[0];
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
