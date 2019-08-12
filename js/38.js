d66dae9cdc85a61b144a887773c43444433d40b8
rednaxela700 <ohleg@windowslive.com>
2019-08-12 11:15:12 +0200

    modify array value

--- js/38.js
+++ js/38.js
@@ -1,12 +1,9 @@
 // Example
-var firstLetterOfFirstName = "";
-var firstName = "Ada";
-
-firstLetterOfFirstName = firstName[0];
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
 
 // Setup
-var firstLetterOfLastName = "";
-var lastName = "Lovelace";
+var myArray = [18,64,99];
 
-// Only change code below this line
-firstLetterOfLastName = lastName[0];
+// Only change code below this line.
+myArray[0] = 45;
