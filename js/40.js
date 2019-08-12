264c4169045a0cc0b2e42d780302c540a0f4da55
rednaxela700 <ohleg@windowslive.com>
2019-08-12 11:17:38 +0200

    array push()

--- js/40.js
+++ js/40.js
@@ -1,12 +1,10 @@
 // Example
-var firstLetterOfFirstName = "";
-var firstName = "Ada";
-
-firstLetterOfFirstName = firstName[0];
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
 
 // Setup
-var firstLetterOfLastName = "";
-var lastName = "Lovelace";
+var myArray = [["John", 23], ["cat", 2]];
 
-// Only change code below this line
-firstLetterOfLastName = lastName[0];
+// Only change code below this line.
+myArray.push(["dog", 3]);
