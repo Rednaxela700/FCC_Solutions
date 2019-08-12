d689e5ee3791be262fe4cc14956b80b8b2c06879
rednaxela700 <ohleg@windowslive.com>
2019-08-12 10:46:58 +0200

    appending variables to strings

--- js/28.js
+++ js/28.js
@@ -1,7 +1,10 @@
-// this is an inline comment
-/*
-this is
-a
-multiline
-comment.
- */
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "a time eater";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
