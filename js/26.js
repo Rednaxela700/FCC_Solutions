bbd2235981d34e437d2d1080398675b5bf0074b1
rednaxela700 <ohleg@windowslive.com>
2019-08-12 10:42:39 +0200

    concatenation with augumented addition

--- js/26.js
+++ js/26.js
@@ -1,7 +1,8 @@
-// this is an inline comment
-/*
-this is
-a
-multiline
-comment.
- */
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence.";
+myStr += " This is the second sentence.";
