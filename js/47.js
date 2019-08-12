e597eeac2d71720ba7fb33ca1eb72571a4b78c60
rednaxela700 <ohleg@windowslive.com>
2019-08-12 11:33:27 +0200

    global scope and functions

--- js/47.js
+++ js/47.js
@@ -1,12 +1,20 @@
-// Example
-var firstLetterOfFirstName = "";
-var firstName = "Ada";
+// Declare your variable here
+var myGlobal = 10;
+var oopsGlobal = 5;
 
-firstLetterOfFirstName = firstName[0];
+function fun1() {
+    // Assign 5 to oopsGlobal Here
 
-// Setup
-var firstLetterOfLastName = "";
-var lastName = "Lovelace";
+}
 
-// Only change code below this line
-firstLetterOfLastName = lastName[0];
+// Only change code above this line
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
