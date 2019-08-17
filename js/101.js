f904e3e0fef96103cc6e7eba309c8d2ad89ca69c
rednaxela700 <ohleg@windowslive.com>
2019-08-17 13:31:59 +0200

    retur a random number between 0-9

--- js/101.js
+++ js/101.js
@@ -1,3 +1,8 @@
-/**
- * Created by alexander on 17/08/2019.
- */
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+    // Only change code below this line.
+
+    return Math.floor(Math.random() * 10);
+}
