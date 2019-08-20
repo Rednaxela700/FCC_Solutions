599570fdf731d82d0b88f576e2c5e0aac97f27c6
rednaxela700 <ohleg@windowslive.com>
2019-08-20 12:39:46 +0200

    generate random whole numbers between range

--- js/102.js
+++ js/102.js
@@ -1,3 +1,18 @@
-/**
- * Created by alexander on 17/08/2019.
- */
+// Example
+function ourRandomRange(ourMin, ourMax) {
+
+    return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
